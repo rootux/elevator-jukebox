cbe918cc861b01f10ce05dbaf4523d0d0fd170fc
Gal Bracha <galbra@gmail.com>
2018-04-01 17:18:31 +0300

    Fixed a bug where music stops. Added 2nd button

--- MusicJukebox.ino
+++ MusicJukebox.ino
@@ -6,17 +6,23 @@
 #include <SoftwareSerial.h>
 #include "Statistic.h"
 
-#define TOTAL_SONGS 24
-#define ACCELEROMETER_TRASHHOLD 0.025
-#define SONG_PLAY_TIME 1000
-#define STD_DEV_SIZE 10
+#define TOTAL_SONGS_FOLDER_1 27
+#define TOTAL_SONGS_FOLDER_2 6
+#define SONG_PLAY_TIME 200 // Time to wait before enabling another press on the button
 // Keeping the speaker from going to sleep mode
 #define TIME_TO_PLAY_SILENCE_TRACK 30000
-const int buttonPin = 3;     // the number of the pushbutton pin
+
 #define ARDUINO_RX 5 //should connect to TX of the Serial MP3 Player module
 #define ARDUINO_TX 6 //connect to RX of the module
 SoftwareSerial mySerial(ARDUINO_RX, ARDUINO_TX);
-int buttonState = 0;         // variable for reading the pushbutton status
+
+// Buttons
+const int firstButtonPin = 8;
+const int secondButtonPin = 9;
+int firstButtonState = 0;
+int secondButtonState = 0;
+int folderToPlayFrom = 0;
+int shouldPlay = false;
 
 static int8_t Send_buf[8] = {0} ;
 
@@ -30,92 +36,143 @@ static int8_t Send_buf[8] = {0} ;
 #define SINGLE_CYCLE_ON 0X00
 #define SINGLE_CYCLE_OFF 0X01
 #define CMD_PLAY_W_VOL 0X22
+#define CMD_PLAY_FLD 0x0F
+
+#define FIRST_FOLDER 0x0100  //Folder 01 on the sd card
+#define SECOND_FOLDER 0x0200 //Folder 02 on the sd card
 
 float lastTimePlayed;
 bool isPlaying = false;
-int currentArrayCount = 0;
-int songsArray[TOTAL_SONGS];
-int songsArraySize = sizeof(songsArray) / sizeof(int);
+
+int currentArrayCountFolder1 = 0;
+int currentArrayCountFolder2 = 0;
+
+int songsArrayFolder1[TOTAL_SONGS_FOLDER_1];
+int songsArrayFolder1Size = sizeof(songsArrayFolder1) / sizeof(int);
+
+int songsArrayFolder2[TOTAL_SONGS_FOLDER_2];
+int songsArrayFolder2Size = sizeof(songsArrayFolder2) / sizeof(int);
 
 void setup()
 {
   // Create songs index array
   // Skipping 1st track as this is the silence track
-  for(int i=1;i< TOTAL_SONGS; i++) {
-    songsArray[i]=i;
+  for(int i=1;i< TOTAL_SONGS_FOLDER_1; i++) {
+    songsArrayFolder1[i]=i;
   }
+  for(int i=1;i< TOTAL_SONGS_FOLDER_2; i++) {
+    songsArrayFolder2[i]=i;
+  }
+
   Serial.begin(115200);
   Serial.println("Started");
   randomSeed(analogRead(2));
   // Fisher-yates shuffle randomly the array
-  shuffle(songsArray, songsArraySize, sizeof(int));
+  shuffle(songsArrayFolder1, songsArrayFolder1Size, sizeof(int));
+  shuffle(songsArrayFolder2, songsArrayFolder2Size, sizeof(int));
 
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
-  digitalWrite(buttonPin, HIGH);
+  pinMode(firstButtonPin, INPUT);
+  digitalWrite(firstButtonPin, HIGH);
+  pinMode(secondButtonPin, INPUT);
+  digitalWrite(secondButtonPin, HIGH);
 
   mySerial.begin(9600);
   delay(500);//Wait chip initialization is complete
-  sendCommand(CMD_SEL_DEV, DEV_TF);//select the TF card  
+  sendCommand(CMD_SEL_DEV, DEV_TF);//select the TF card
   delay(200);
   lastTimePlayed = -1 * SONG_PLAY_TIME;
+  sendCommand(CMD_SEL_DEV, 0x001E); //Set volume to max
+  delay(200);
 }
 
 void test() {
-  for(int i=0;i<TOTAL_SONGS;i++) {
+  for(int i=0;i<TOTAL_SONGS_FOLDER_1;i++) {
     Serial.print("Play a song "); Serial.println(i);
-    sendCommand(CMD_PLAY_W_VOL, 0X1E01+(i*2));
+    sendCommand(CMD_PLAY_FLD, FIRST_FOLDER + 0x01 + (i));
     delay(3000);
   }
 }
 
-void loop() 
-{ 
+void randomizeNewSongsArray1() {
+  currentArrayCountFolder1 = 0;
+  randomSeed(analogRead(2));
+  shuffle(songsArrayFolder1, songsArrayFolder1Size, sizeof(int));
+}
+
+void randomizeNewSongsArray2() {
+  currentArrayCountFolder2 = 0;
+  randomSeed(analogRead(2));
+  shuffle(songsArrayFolder2, songsArrayFolder2Size, sizeof(int));
+}
+
+void loop()
+{
   // Check if should randomize new songs array
-  if(currentArrayCount >= TOTAL_SONGS) {
-    currentArrayCount = 0;
-    randomSeed(analogRead(2));
-    shuffle(songsArray, songsArraySize, sizeof(int));
+  if(currentArrayCountFolder1 >= TOTAL_SONGS_FOLDER_1) {
+    randomizeNewSongsArray1();
   }
 
-  // TODO Check if button pressed
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+  if(currentArrayCountFolder2 >= TOTAL_SONGS_FOLDER_2) {
+    randomizeNewSongsArray2();
+  }
 
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == LOW) { 
-      
+  // read the state of the pushbutton value:
+  firstButtonState = digitalRead(firstButtonPin);
+  secondButtonState = digitalRead(secondButtonPin);
+
+  // Check if any of the buttons were pressed
+  if (firstButtonState == LOW) {
+    folderToPlayFrom = FIRST_FOLDER;
+    shouldPlay = true;
+  }else {
+    shouldPlay = false;
+  }
+
+  if (secondButtonState == LOW) {
+    folderToPlayFrom = SECOND_FOLDER;
+    shouldPlay = true;
+  }
+
+  if(shouldPlay) {
     if(millis() - lastTimePlayed >= SONG_PLAY_TIME) {
       lastTimePlayed = millis();
       isPlaying = true;
-      int8_t randSong = songsArray[currentArrayCount++];
+
+      int8_t randSong = 0;
+      if(folderToPlayFrom == FIRST_FOLDER) {
+        randSong = songsArrayFolder1[currentArrayCountFolder1++];
+      }else if(folderToPlayFrom == SECOND_FOLDER) {
+        randSong = songsArrayFolder2[currentArrayCountFolder2++];
+      }
+
+      Serial.print("Play a song "); Serial.println(randSong + 1);
+      sendCommand(CMD_PLAY_FLD, folderToPlayFrom + 0x01 + (randSong));
       delay(50);
-      Serial.print("Play a song "); Serial.println(randSong);
-      sendCommand(CMD_PLAY_W_VOL, 0X1E01 + (randSong * 2)); //TODO Not sure why *2
     } else {
       Serial.println("Already playing");
     }
 
     // Should stop song
-    if(isPlaying && millis() - lastTimePlayed >= SONG_PLAY_TIME) {
-      isPlaying = false;
-      Serial.println("Stop song");
-      sendCommand(CMD_PAUSE, 0);
-    }
+    //if(isPlaying && millis() - lastTimePlayed >= SONG_PLAY_TIME) {
+    //  isPlaying = false;
+    //  Serial.println("Stop song");
+    //  sendCommand(CMD_PAUSE, 0);
+    //}
 
-    bool notPlaying = ((millis() - lastTimePlayed) > SONG_PLAY_TIME);
-    if (notPlaying && (millis() % TIME_TO_PLAY_SILENCE_TRACK) > TIME_TO_PLAY_SILENCE_TRACK - 1000) {
-        Serial.println("Playing silence to stay awake");
-        sendCommand(CMD_PLAY_W_VOL, 0X0315); //Playing some song (21 0x15) with low volumes (03)
-        delay(600);
-        sendCommand(CMD_PAUSE, 0);
-        delay(50);
-        sendCommand(CMD_PAUSE, 0);
-    }
+    // The following prevent sleeping
+//    bool notPlaying = ((millis() - lastTimePlayed) > SONG_PLAY_TIME);
+//    if (notPlaying && (millis() % TIME_TO_PLAY_SILENCE_TRACK) > TIME_TO_PLAY_SILENCE_TRACK - 1000) {
+//        Serial.println("Playing silence to stay awake");
+//        sendCommand(CMD_PLAY_W_VOL, 0X0315); //Playing some song (21 0x15) with low volumes (03)
+//        delay(600);
+//        sendCommand(CMD_PAUSE, 0);
+//        delay(50);
+//        sendCommand(CMD_PAUSE, 0);
+//    }
   }
 
-  // delay(50);
+  shouldPlay = false;
 }
 void sendCommand(int8_t command, int16_t dat)
 {
